0b9a66dd16a6fcd959628c1176a01b5b3ec380f3
Manishka Fonseka <manishkafon@gmail.com>
2023-03-10 19:33:49 +0530

    Completed upto part B task 11

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,23 @@
 public class Person {
-    public static void main(String[] args) {
+    private String name;
+    private String surname;
+    private String email;
 
+    public Person(String name,String sName,String email) {
+        this.name = name;
+        surname = sName;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
---
+++ src/Ticket.java
@@ -0,0 +1,24 @@
+public class Ticket {
+    private int row;
+    private int seat;
+    private double price;
+    private Person person;
+
+
+    public Ticket(int row, int seat, double price, Person person) {
+        this.row = row;
+        this.seat = seat;
+        this.price = price;
+        this.person = person;
+    }
+
+    public void print() {
+        System.out.println("Name : " + person.getName());
+        System.out.println("Surname : " +person.getSurname());
+        System.out.println("Email : " + person.getEmail());
+        System.out.println("Seat row : "+ row);
+        System.out.println("Seat No. : "+ seat);
+        System.out.println("price : "+ price);
+    }
+
+}
